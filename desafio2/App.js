a53adaf330b14393aafa67f3512fd74eec4fea00
henriqueweiand <henrique.weiand@avantis.edu.br>
2017-11-17 10:51:38 -0200

    desafio2 base projeto com plugins

---
+++ desafio2/App.js
@@ -0,0 +1,22 @@
+
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+export default class App extends Component {
+  componentWillMount = () => {
+    // teste
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>
+          teste
+        </Text>
+      </View>
+    );
+  }
+}
