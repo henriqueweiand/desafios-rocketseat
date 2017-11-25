faa4d39c9618517c7bcb11866eee21a6a9cc777c
henriqueweiand <henrique.weiand@avantis.edu.br>
2017-11-25 10:59:37 -0200

    alteracoes no projeto todo list com redux

--- todoList/redux/src/actions/todos.js
+++
@@ -1,8 +0,0 @@
-export function addTodo(text) {
-  return {
-    type: 'ADD_TODO',
-    payload: {
-      text,
-    },
-  }
-}
--- todoList/redux/src/reducers/todos.js
+++
@@ -1,8 +0,0 @@
-export default function todos(state = [], action) {
-  switch (action.type) {
-    case 'ADD_TODO':
-      return [ ...state, { id: Math.random(), text: action.payload.text } ];
-    default:
-      return state;
-  }
-}
